f9c4a450858ab626ddd9208e61e00e6933d08ed9
Subashri <subashri.a@gmail.com>
2022-03-28 17:15:56 -0400

    modified hello.java by dev2

--- hello.java
+++ hello.java
@@ -2,4 +2,11 @@ now checking git stash can be restored after commit
 {
 	Checking staged files can be restored
 }
+Learning conflict
+{
+	Public Static void main ()
+	{
+		System.out.println ("hello")
+	}
+}
 
